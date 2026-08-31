a177bb2a64488cfd912636da95f7fe4a94ca3963
Jorink <maxjorink@gmail.com>
2026-08-31 20:45:22 +0200

    Fix small naming mistake

--- Mod/Properties/AssemblyInfo.cs
+++ Mod/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using MelonLoader;
 using healthregentoggle;
 
-[assembly: MelonInfo(typeof(HealthRegenToggle), "HealthRegenToggle", HRT.Version, "Jorink")]
+[assembly: MelonInfo(typeof(HealthRegenToggle), "HealthRegenToggle", HealthRegenToggle.Version, "Jorink")]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
 [assembly: MelonColor(0,255,0,1)]
 [assembly: MelonAuthorColor(255,0,0,1)]
