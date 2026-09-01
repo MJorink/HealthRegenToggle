f826533f319857d2491f8a356df6ae87720543ce
Jorink <maxjorink@gmail.com>
2026-09-01 22:56:30 +0200

    Fix AssemblyInfo colors

--- Mod/Properties/AssemblyInfo.cs
+++ Mod/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using healthregentoggle;
 
 [assembly: MelonInfo(typeof(HealthRegenToggle), "HealthRegenToggle", HealthRegenToggle.Version, "Jorink")]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
-[assembly: MelonColor(0,255,0,1)]
-[assembly: MelonAuthorColor(255,0,0,1)]
+[assembly: MelonColor(255,0,255,0)]
+[assembly: MelonAuthorColor(255,255,0,0)]
